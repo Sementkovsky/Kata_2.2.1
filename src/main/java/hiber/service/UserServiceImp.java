0a3269157a314f06ad24148792be2bbe771b0646
Alexey Sementkovskiy <sementkovsky@yandex.ru>
2022-10-09 16:58:23 +0300

    My Solution Ver3

--- src/main/java/hiber/service/UserServiceImp.java
+++ src/main/java/hiber/service/UserServiceImp.java
@@ -11,24 +11,22 @@ import javax.persistence.Query;
 import java.util.List;
 
 @Service
+@Transactional
 public class UserServiceImp implements UserService {
 
    @Autowired
    private UserDao userDao;
 
-   @Transactional
    @Override
    public void add(User user) {
       userDao.add(user);
    }
 
-   @Transactional(readOnly = true)
    @Override
    public List<User> listUsers() {
       return userDao.listUsers();
    }
 
-   @Transactional(readOnly = true)
    @Override
    public User getUserByCar (String model, int series)  {
       return userDao.getUserByCar(model, series);
