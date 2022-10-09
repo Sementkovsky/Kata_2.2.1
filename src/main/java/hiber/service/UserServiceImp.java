78f348e1abcfb45bc8a82220222e9e1b334fb3f7
Alexey Sementkovskiy <sementkovsky@yandex.ru>
2022-10-09 17:52:48 +0300

    My Solution Ver4

--- src/main/java/hiber/service/UserServiceImp.java
+++ src/main/java/hiber/service/UserServiceImp.java
@@ -3,6 +3,7 @@ package hiber.service;
 import hiber.dao.UserDao;
 import hiber.model.Car;
 import hiber.model.User;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,21 +12,27 @@ import javax.persistence.Query;
 import java.util.List;
 
 @Service
-@Transactional
 public class UserServiceImp implements UserService {
 
-   @Autowired
    private UserDao userDao;
+   @Autowired
+   public UserServiceImp(UserDao userDao) {
+      this.userDao = userDao;
+   }
 
+
+   @Transactional
    @Override
    public void add(User user) {
       userDao.add(user);
    }
 
+   @Transactional(readOnly = true)
    @Override
    public List<User> listUsers() {
       return userDao.listUsers();
    }
+
 
    @Override
    public User getUserByCar (String model, int series)  {
